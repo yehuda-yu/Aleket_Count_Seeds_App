4d4d2ac0243342ce0dda2b879a4a3da0cb28cd8f
yehuda yungstein <81097407+yehuda-yu@users.noreply.github.com>
2022-12-28 18:16:17 +0200

    Update Aleket_streamlit.py

--- Aleket_streamlit.py
+++ Aleket_streamlit.py
@@ -43,11 +43,7 @@ def segment_image_kmeans(img, k=3, attempts=10):
 st.set_page_config(page_icon = "bar_chart", layout = 'wide')
 st.title("Image KMeans Clustering")
 with st.expander("See explanation"):
-    st.write(\"\"\"
-        The chart above shows some numbers I picked for you.
-        I rolled actual dice for these, so they're *guaranteed* to
-        be random.
-    \"\"\")
+    st.write("the chart above shows some numbers I picked for you.rolled actual dice for these, so they're *guaranteed* to be random.")
     st.image("https://static.streamlit.io/examples/dice.jpg")
 # Allow the user to select a TIF image file
 uploaded_file = st.file_uploader("Choose image file")
