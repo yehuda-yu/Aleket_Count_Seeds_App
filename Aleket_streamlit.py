ca388b1341b92c9bf25aa9b023a28de13e376dff
yehuda yungstein <81097407+yehuda-yu@users.noreply.github.com>
2022-12-28 21:55:59 +0200

    Update Aleket_streamlit.py

--- Aleket_streamlit.py
+++ Aleket_streamlit.py
@@ -67,15 +67,15 @@ if uploaded_file is not None:
         # Median filter on L channel to clean Noise
         st.write('2. Apply Median filter')
         # Apply the median filter with a radius of 2 to the L channel of the CIE LAB image using the median function from skimage:
-        image_lab[:,:,2] = scipy.ndimage.median_filter(image_lab[:,:,2], footprint=np.ones((2,2)))
-        st.image(image_lab[:,:,2],clamp=True,caption='L channel image',)
+        L_channel = scipy.ndimage.median_filter(image_lab[:,:,2], footprint=np.ones((2,2)))
+        st.image(L_channel,clamp=True,caption='L channel image',)
     
         # Calculate the Otsu threshold and create masked image
         st.write('3. Mask with Otsu threshold')
-        threshold = threshold_otsu(image_lab[:,:,2])
+        threshold = threshold_otsu(L_channel)
         # Create a mask using the Otsu threshold
-        mask = image_lab[:,:,2] > threshold
-        st.image(mask,clamp=True,caption='Masked image',width=500)
+        mask = L_channel > threshold
+        st.image(mask,clamp=True,caption='Masked image',)
 
         ###################### Kmens ######################
 
