cf1e1d16a74ace3d32e96529dad523f3cea868ca
yehuda yungstein <81097407+yehuda-yu@users.noreply.github.com>
2022-12-28 21:41:39 +0200

    Update Aleket_streamlit.py

--- Aleket_streamlit.py
+++ Aleket_streamlit.py
@@ -61,7 +61,7 @@ if uploaded_file is not None:
         st.write('1. Convert from RGB to CIE-LAB format')
         image = io.imread(uploaded_file)
         image_lab = color.rgb2lab(image)
-        st.image(image_lab,clamp=False,caption="LAB format image",width=500)
+        st.image(image_lab,clamp=True,caption="LAB format image",width=500)
 
         # Median filter on L channel to clean Noise
         st.write('2. Apply Median filter')
