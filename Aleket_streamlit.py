18d2ce233fc6fdfa8639449316fb6f2231d6f2a0
yehuda yungstein <81097407+yehuda-yu@users.noreply.github.com>
2023-02-20 13:32:28 +0200

    Update Aleket_streamlit.py

--- Aleket_streamlit.py
+++ Aleket_streamlit.py
@@ -24,35 +24,6 @@ import time
 
 ###################### Functions ######################
 @st.cache
-# function for k mean
-def segment_image_kmeans(img, k=3, attempts=10): 
-
-    # Convert MxNx3 image into Kx3 where K=MxN
-    pixel_values  = img.reshape((-1,3))  #-1 reshape means, in this case MxN
-
-    #We convert the unit8 values to float as it is a requirement of the k-means method of OpenCV
-    pixel_values = np.float32(pixel_values)
-
-    # define stopping criteria
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
-    
-    _, labels, (centers) = cv2.kmeans(pixel_values, k, None, criteria, attempts, cv2.KMEANS_RANDOM_CENTERS)
-    
-    # convert back to 8 bit values
-    centers = np.uint8(centers)
-
-    # flatten the labels array
-    labels = labels.flatten()
-    
-    # convert all pixels to the color of the centroids
-    segmented_image = centers[labels.flatten()]
-    
-    # reshape back to the original image dimension
-    segmented_image = segmented_image.reshape(img.shape)
-    
-    return segmented_image
-
-
 def process_image(image):
     # Convert to PIE-LaB format:
     image_lab = color.rgb2lab(image)
@@ -148,16 +119,4 @@ if uploaded_file is not None:
         # StarDist:
         st.subheader('StarDist prediction')
         StarDist_prediction(L_channel*cleaned_image)
-        
-        
-        ###################### Kmens ######################
-
-    # slider for choosing K:
-    k_value = st.slider('Insert K value (number of clusters):', 2,10,3) # asks for input from the user
-    attempts_value_slider = st.slider('Number of attempts', value = 7, min_value = 1, max_value = 10) # slider example
-    segmented_image = segment_image_kmeans(image, k=k_value, attempts=attempts_value_slider)
-    
-    # Display the result
-    st.subheader('Output Image')
-    st.image(segmented_image, use_column_width=True)
-
+ 
